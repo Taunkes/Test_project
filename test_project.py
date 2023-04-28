0301673fc292ec2c2d961ff276886ddd251a22e1
Taunkes <125264184+Taunkes@users.noreply.github.com>
2023-04-28 18:46:29 +0300

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,9 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-import telebot
 
 
-api = '5871606488:AAGwfme2pM4XZKgX1efJQBFNA98O0zS0yzo'
 url = 'https://mangalib.me/?section=home-updates'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser') #Парсинг всей страницы
@@ -16,14 +14,6 @@ for data in new_tittles: # НАЗВАНИЯ
         if data.find('a', class_='link-default') is not None: #Если название не пустая
             tittles_list.append(data.text) #Добавление в список
 
-tittles_list = list(map(lambda x: x.strip(), tittles_list)) #Сортировка
+tittles_list = list(map(lambda x: x.strip(), tittles_list)).replace('[', '').replace #Сортировка
 
-# print(f'tittles list = {tittles_list}') #Принт в консоль для проверки полученых файлов
-
-
-def searching(user_inp):
-    if str(user_inp) in tittles_list:
-        return 'Вышла новая глава'
-    else:
-        return 'Новой главы не обнаруженно'
-
+print(f'tittles list = {tittles_list}') #Принт в консоль для проверки полученых файлов
