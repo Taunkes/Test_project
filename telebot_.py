3125df44274fc2d56773234b15a0ef9e295693ae
Taunkes <125264184+Taunkes@users.noreply.github.com>
2023-04-28 20:16:22 +0300

    Update telebot_.py

--- telebot_.py
+++ telebot_.py
@@ -14,13 +14,20 @@ def message_reply(message):
         bot.send_message(message.chat.id, 'https://mangalib.me/?section=home-updates')
     
     elif message.text == '/list':
-       for x in range(0, len(tittles_list), 4096):
-            bot.send_message(message.chat.id, tittles_list[x:x+4096])
+        for x in range(0, len(tittles_list), 4096):
+            bot.send_message(message.chat.id, f'{tittles_list[x:x+4096]}')
     
     elif message.text == '/l':
         bot.send_animation(message.chat.id, 'https://media.tenor.com/-IO8NDvwkDAAAAAC/walter-l-walter-white.gif')
     
     elif message.text in ['Привет', '/start', 'Hi', 'Хай']:
-        bot.send_message(message.chat.id, 'Привет! Используй одну из команд:')
+        bot.send_message(message.chat.id, 'Привет! Используй одну из команд или напиши название тайтла:')
+    
+    elif message.text in tittles_list:
+        bot.send_message(message.chat.id, 'Этот тайтл недавно обновлён')
+
+    else:
+        bot.send_message(message.chat.id, 'Бот не понимает\nhttps://media.tenor.com/qkPV6_DL-NAAAAAd/bocchi-the-rock-bocchi.gif')
+
 
 bot.infinity_polling()
