8de78097f64f8773d65cd5778a381682892b08f6
Taunkes <125264184+Taunkes@users.noreply.github.com>
2023-04-28 18:46:06 +0300

    Update telebot_.py

--- telebot_.py
+++ telebot_.py
@@ -1,20 +1,26 @@
 import telebot
+from test_project import tittles_list
+
+
 '''Creating the bot'''
 
-api = '5871606488:AAGwfme2pM4XZKgX1efJQBFNA98O0zS0yzo'
+api = '5871606488:AAG9Q014OrB75p_31zkFR3jWlErAHv28s7I'
 bot = telebot.TeleBot(api)
 
-@bot.message_handler(commands=['/start', 'Привет'])
-def hello(message):
-    bot.send_message(message.chat.id, 'Привет! Используй одну из команд:')
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    
+    if message.text == '/link':
+        bot.send_message(message.chat.id, 'https://mangalib.me/?section=home-updates')
+    
+    elif message.text == '/list':
+       for x in range(0, len(tittles_list), 4096):
+            bot.send_message(message.chat.id, tittles_list[x:x+4096])
+    
+    elif message.text == '/l':
+        bot.send_animation(message.chat.id, 'https://media.tenor.com/-IO8NDvwkDAAAAAC/walter-l-walter-white.gif')
+    
+    elif message.text in ['Привет', '/start', 'Hi', 'Хай']:
+        bot.send_message(message.chat.id, 'Привет! Используй одну из команд:')
 
-@bot.message_handler(content_types=['text'])
-def words(message):
-    if message == '/command1':
-        bot.send_message(message.chat.id, 'Введите название(not работает)')
-    elif message == '/command2':
-        bot.send_message(message.chat.id, 'Пока не работает')
-    else:
-        'Пока что бот не распознаёт слова'
-
-bot.polling(none_stop=True)
+bot.infinity_polling()
